0b0f4cdfc0d61c1af5ab697245103d6109fb4d25
ITP <RayhanUddinFarhad11@gmail.com>
2023-08-29 12:48:15 +0600

    change route

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const Port = process.env.PORT || 5000
 
 const cors = require('cors');
 const connectDB = require('./config/db');
-const postRoute = require('./routes/post.route.js');
-const userRoute = require('./routes/users.route.js');
+const postRoute = require('./routes/post.route');
+const userRoute = require('./routes/users.route');
 
 app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use (postRoute)
-app.use(userRoute)
+app.use ( '/api/post',  postRoute)
+app.use('/api/users', userRoute)
 
 
 app.listen (Port, async() => {
