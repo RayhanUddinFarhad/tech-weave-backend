f2e41dbc5c4a2da0398fc6dd9664bde564de1751
ITP <RayhanUddinFarhad11@gmail.com>
2023-08-29 15:52:09 +0600

    updated

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use ( '/api/users', postRoute)
-app.use( '/api/post', userRoute)
+app.use ( "/", postRoute)
+app.use( "/", userRoute)
 
 
 app.listen (Port, async() => {
